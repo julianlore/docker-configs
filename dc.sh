88cf46a259f545dc2d2781b65e09db640f012f1f
Julian Lore <julian.lore@mail.mcgill.ca>
2021-10-04 22:24:17 -0400

    Remove wg_ub from dc.sh since it needs to create the network first

--- dc.sh
+++ dc.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Runs docker-compose with the files to use by default with specified args
-YMLFILES=( "caddy" "bitwarden" "syncthing" "wg_ub" )
+YMLFILES=( "caddy" "bitwarden" "syncthing" )
 for i in "${YMLFILES[@]}"
 do
   YMLARG=$YMLARG" -f "$i".yml"
