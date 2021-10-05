09c3205698f0776eec3b8c0b6965408a0b3457d7
Julian Lore <julian.lore@mail.mcgill.ca>
2021-10-04 22:15:21 -0400

    Add wg_ub and gluetun to dc.sh

--- dc.sh
+++ dc.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Runs docker-compose with the files to use by default with specified args
-YMLFILES=( "caddy.yml" "bitwarden.yml" "syncthing.yml" )
+YMLFILES=( "caddy.yml" "bitwarden.yml" "syncthing.yml" "wg_ub.yml" "gluetun.yml" )
 for i in "${YMLFILES[@]}"
 do
   YMLARG=$YMLARG" -f "$i
