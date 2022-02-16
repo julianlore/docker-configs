1638d1fb23442b49d09d280e73c6484509a34f10
Julian Lore <julian.lore@mail.mcgill.ca>
2022-02-16 05:06:52 +0000

    Add speedtest, agh, uptime-kuma, nginx, gt_ub and red to dc.sh

--- dc.sh
+++ dc.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Runs docker-compose with the files to use by default with specified args
-YMLFILES=( "caddy" "bitwarden" "syncthing" "organizr" "netdata" )
+YMLFILES=( "caddy" "bitwarden" "syncthing" "organizr" "netdata" "speedtest" "agh" "uptime-kuma" "nginx" "gt_ub" "red")
 for i in "${YMLFILES[@]}"
 do
   YMLARG=$YMLARG" -f "$i".yml"
