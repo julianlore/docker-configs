1c3d6ad78c5055a0facf36e445ddaa26245e17eb
Julian Lore <julian.lore@mail.mcgill.ca>
2022-05-05 22:40:38 -0400

    Remove gt_ub and uptime_kuma from dc.sh as they are restarted separately, add acme.yaml with acme.sh and ofelia for automatic cert renewal

--- dc.sh
+++ dc.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Runs docker-compose with the files to use by default with specified args
-YMLFILES=( "caddy" "bitwarden" "syncthing" "organizr" "netdata" "speedtest" "agh" "uptime-kuma" "nginx" "gt_ub" "red")
+YMLFILES=( "caddy" "bitwarden" "syncthing" "organizr" "netdata" "speedtest" "agh" "nginx" "red")
 for i in "${YMLFILES[@]}"
 do
   YMLARG=$YMLARG" -f "$i".yml"
