bdabea89a9ce261a3f0f255afc85717d96a96519
Julian Lore <julian.lore@mail.mcgill.ca>
2022-06-29 22:13:00 -0400

    Remove caddy from dc.sh to bring it up/down separately.

--- dc.sh
+++ dc.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Runs docker-compose with the files to use by default with specified args
-YMLFILES=( "caddy" "bitwarden" "syncthing" "organizr" "netdata" "speedtest" "agh" "nginx" "red" "linkding" "acme")
+YMLFILES=( "bitwarden" "syncthing" "organizr" "netdata" "speedtest" "agh" "nginx" "red" "linkding" "acme")
 for i in "${YMLFILES[@]}"
 do
   YMLARG=$YMLARG" -f "$i".yml"
