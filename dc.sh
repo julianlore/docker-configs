bab0ac59f641d6a3c00622a222405bfc010c334c
Julian Lore <julian.lore@mail.mcgill.ca>
2024-03-22 22:26:16 -0400

    Remove nginx and radicale from dc.sh

--- dc.sh
+++ dc.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Runs docker-compose with the files to use by default with specified args
-YMLFILES=( "bitwarden" "syncthing" "organizr" "netdata" "speedtest" "agh" "nginx" "red" "acme" "radicale" "couchdb")
+YMLFILES=( "bitwarden" "syncthing" "netdata" "speedtest" "agh" "red" "acme" "couchdb")
 for i in "${YMLFILES[@]}"
 do
   YMLARG=$YMLARG" -f "$i".yml"
